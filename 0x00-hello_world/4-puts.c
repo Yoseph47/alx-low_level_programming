f11df54d15e0546ac0690fd19d2978d8c1c54877
Yoseph47 <jossyseyoum@gmail.com>
2022-06-30 19:57:22 -0700

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+/*
+*header file
+*main - simple display statement using puts
+*Return: always 0
+*/
+int main(void)
+/*
+*main function
+*/
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
