ed518e9f516f8d3277acb722094e8099d078666f
Yoseph47 <jossyseyoum@gmail.com>
2022-07-01 22:32:27 -0700

    puts print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-
 #include <stdio.h>
 /**
-*header file
-*main - simple display statement using puts
-*Return: always 0
+* main - simple display statement using puts
+*
+* Return: always 0
 */
 int main(void)
-/*
-*main function
-*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
