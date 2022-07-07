7e54cc2dea07eeac13dccf902fe8a41476693cae
Yoseph47 <jossyseyoum@gmail.com>
2022-07-07 18:24:36 +0300

    bracket

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
 if(c >= 64 && c <= 91)
 {
-    return 1;
+    return (1);
 }
-    return 0;
+    return (0);
 }
