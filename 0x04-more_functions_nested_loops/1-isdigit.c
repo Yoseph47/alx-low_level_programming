9c59cbf9f436fa162d4404a9534187e98bb9ecf9
Yoseph47 <jossyseyoum@gmail.com>
2022-07-08 15:47:57 +0300

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-/**
-* _isupper -> this is a function to check upper case alphas
-* @c: an integer paramer
-* Return: something but success
-*/
-int _isupper(int c)
+int _isdigit(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+if ( c >= 48 && c <= 59 )
+{
+return (1);
 }
+else
+{
+return (0);
+}
+}
+
+
