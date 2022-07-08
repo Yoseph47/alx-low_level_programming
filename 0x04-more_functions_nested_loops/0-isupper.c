4098e79ab1f8f151a51ba01561d30a57c90b4667
Yoseph47 <jossyseyoum@gmail.com>
2022-07-08 16:09:49 +0300

    checks if alphabet is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - check if alphabet c uppercase code.
-*
-* Return: Always 0.
-*/
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
 int _isupper(int c)
 {
-if ( c >= 65 && c <= 90 )
+if (c >= 65 && c <= 90)
 {
 return (1);
 }
