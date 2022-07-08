cc86945226d48b384388525c0c96f912d26998a9
Yoseph47 <jossyseyoum@gmail.com>
2022-07-08 01:26:29 -0700

    checks if c is uppercase or lowerd case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,7 @@
 #include "main.h"
 /**
+* _isupper - checks if parameter is an uppercase character.
+* @c: input character.
 * main - check the code.
 *
 * Return: Always 0.
@@ -8,5 +10,10 @@ int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 {
-return (c >= 'A' && c <= 'Z');
+return (1);
+}
+else
+{
+return (0);
+}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,11 @@
+#ifndef main.h
+#define main.h
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+
+
+
+
+#endif
