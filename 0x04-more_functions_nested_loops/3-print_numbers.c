d7faa5f2583ac2f55366c9464a3a4d896b446485
Yoseph47 <jossyseyoum@gmail.com>
2022-07-08 17:24:22 +0300

    prints number 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,24 @@
 #include "main.h"
+// #include <unistd.h>
 /**
 * print_numbers -> print numbers
+*Return: no return.
 */
 void print_numbers(void)
 {
 int ch;
-for (ch = 48; ch < 58; ch++)
+for (ch = 48; ch <= 57; ch++)
 {
 _putchar(ch);
 _putchar('\n');
 }
 }
+// int _putchar(char c)
+// {
+// 	return (write(1, &c, 1));
+// }
+// int main(void)
+// {
+//     print_numbers();
+//     return (0);
+// }
