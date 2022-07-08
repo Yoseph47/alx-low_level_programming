008fe9e7d787ffa15e969da36f4f24be9670355e
Yoseph47 <jossyseyoum@gmail.com>
2022-07-08 17:36:46 +0300

    adds number from 0 to 9 in a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-// #include <unistd.h>
+
 /**
 * print_numbers -> print numbers.
 *Return: no return.
@@ -13,12 +13,3 @@ _putchar(ch);
 }
 _putchar('\n');
 }
-// int _putchar(char c)
-// {
-// 	return (write(1, &c, 1));
-// }
-// int main(void)
-// {
-//     print_numbers();
-//     return (0);
-// }
