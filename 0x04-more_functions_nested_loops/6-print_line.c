288557917a6cb21d209ecfaa70fbe55a1529bcfc
Yoseph47 <jossyseyoum@gmail.com>
2022-07-09 05:23:34 +0300

    prints diagonal $ sign

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_line - prints straight line n times.
-* @n: number of times straight line is printed.
+* @n: number straight line is printed.
 * Return: no return because its void.
 */
 void print_line(int n)
