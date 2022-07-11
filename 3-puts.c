80c313880a0a4af6589642e2c218c2401e0b7cba
Yoseph47 <jossyseyoum@gmail.com>
2022-07-12 00:27:54 +0300

    prints a string

--- 3-puts.c
+++ 3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string.
+ * _puts - prints a string, followed by a new linee.
+ * @str: input  string.
  * Return: no return value.
  */
 void _puts(char *str)
