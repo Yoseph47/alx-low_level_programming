8078053afac114fcfc5ccf2aaf929d7c43f7b80c
Yoseph47 <jossyseyoum@gmail.com>
2022-07-14 21:35:13 +0300

    function that concatenates two strings. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
- * _strncpy - function to copy string from source to destination
- * @dest: pointer to destinatioin of string to be copied
- * @src: pointer to source of string to be copied
- * @n: int type to determine size of dest to copy into
- * Return: return dest
- */
-char *_strncpy(char *dest, char *src, int n)
+  * _strncat - appends a maximum of n characters from src to dest
+  * @dest: string to be appended to
+  * @src: string to append from
+  * @n: maximum number of bytes to be copied from src
+  * Return: pointer to the resulting dest string
+  */
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int index, length;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	length = 0;
+	while (dest[length] != '\0')
+		length++;
+
+	index = 0;
+	while (index < n && src[index] != '\0')
+	{
+		dest[length] = src[index];
+		length++;
+		index++;
+	}
 	return (dest);
 }
