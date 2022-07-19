a8b423d2eadb6780cd7951efc4d5d6d014288bf9
Yoseph47 <jossyseyoum@gmail.com>
2022-07-19 04:40:56 +0300

    copy from  memory area to another memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
-  * _memcpy - copies n bytes from one memory area to another
+  * _memcpy - copies n bytes from  memory area to another memory
   * @dest: destination memory area
   * @src: source memory area
   * @n: number of bytes to be copied
