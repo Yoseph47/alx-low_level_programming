eb18709da58743a3f44fa58b201b9c413891acff
Yoseph47 <jossyseyoum@gmail.com>
2022-07-26 19:17:58 +0300

    function that returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 /**
  * create_array - creates an array of chars.
  * @size: size of the array.
- * @c: storaged char
+ * @c: storaged characters
  *
  * Return: pointer of an array of chars
  */
@@ -11,6 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
 	cr = malloc(sizeof(c) * size);
